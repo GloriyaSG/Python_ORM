28fa91ede4b4376fd9e65ad4216886229795b251
Gloriya Gerginova <122824646+GloriyaSG@users.noreply.github.com>
2023-10-24 23:32:25 +0300

    migrate updated models

--- Django Models Bsics - Exercise/01. Person_Skeleton/main_app/models.py
+++ Django Models Bsics - Exercise/01. Person_Skeleton/main_app/models.py
@@ -111,3 +111,35 @@ class WeatherForecast(models.Model):
 
     precipitation = models.FloatField()
 
+class Recipe(models.Model):
+
+    name = models.CharField(
+        max_length=100,
+        unique=True)
+
+    description = models.TextField()
+
+    ingredients = models.TextField()
+
+    cook_time = models.PositiveIntegerField()
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+class Product(models.Model):
+    name = models.CharField(
+        max_length=70
+    )
+    description = models.TextField()
+
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+

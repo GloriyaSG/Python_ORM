b2e487e4371b2ab14aaece4f60ffef3d09b8874a
Gloriya Gerginova <122824646+GloriyaSG@users.noreply.github.com>
2023-10-26 22:16:16 +0300

    migrate updated models

--- Django Models Bsics - Exercise/01. Person_Skeleton/main_app/models.py
+++ Django Models Bsics - Exercise/01. Person_Skeleton/main_app/models.py
@@ -142,4 +142,89 @@ class Product(models.Model):
         auto_now_add=True
     )
 
+class UserProfile(models.Model):
+    username = models.CharField(
+        max_length=65,
+        unique=True
+    )
+    first_name = models.CharField(
+        max_length=40,
+        unique=True
+    )
+    last_name = models.CharField(
+        max_length=40,
+        unique=True
+    )
+    email = models.EmailField(
+        unique=True,
+        default= "students@softuni.bg"
+    )
+    bio = models.TextField(
+        max_length=120
+    )
+    profile_image_url = models.URLField()
 
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+class Exercise(models.Model):
+    name = models.CharField(
+        max_length=100
+    )
+    description = models.TextField()
+
+    difficulty_level = models.CharField(
+        max_length=20
+    )
+    duration_minutes = models.PositiveIntegerField()
+
+    equipment = models.CharField(
+        max_length= 90
+    )
+    video_url = models.URLField(
+        null=True,
+        blank=True
+    )
+    calories_burned = models.PositiveIntegerField(
+        default=0
+    )
+    is_favorite = models.BooleanField(
+        default=False
+    )
+
+class Book(models.Model):
+    genre_choices = [
+        ("Fiction", "Fiction"),
+        ("Non-Fiction", "Non-Fiction"),
+        ("Science Fiction", "Science Fiction"),
+        ("Horror", "Horror")
+    ]
+
+    title = models.CharField(
+        max_length=30
+    )
+    author = models.CharField(
+        max_length=100
+    )
+    genre = models.CharField(
+        max_length=20,
+        choices= genre_choices
+    )
+    publication_date = models.DateField(
+        editable=False,
+        auto_now_add=True
+    )
+    price = models.DecimalField(
+        max_digits=8,
+        decimal_places=2
+    )
+    is_available = models.BooleanField(
+        default=True
+    )
+    rating = models.FloatField()
+
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title

f5a92156b52c5c02cd8526e61fe180b0a534694c
Gloriya Gerginova <122824646+GloriyaSG@users.noreply.github.com>
2023-11-02 23:02:07 +0200

    migrate updated models

--- Migrations_and_Django_Exercise/main_app/models.py
+++ Migrations_and_Django_Exercise/main_app/models.py
@@ -15,11 +15,8 @@ class UniqueBrands(models.Model):
 class EventRegistration(models.Model):
     event_name = models.CharField(
         max_length=60
-    ),
+    )
     participant_name = models.CharField(
         max_length=50
-    ),
+    )
     registration_date = models.DateField()
-
-    def __str__(self):
-        return f"{self.participant_name} - {self.event_name}"

c7538973053155c4830ab8773ad2be0861e799e2
Gloriya Gerginova <122824646+GloriyaSG@users.noreply.github.com>
2023-11-04 00:05:28 +0200

    migrate updated models

--- Migrations_and_Django_Exercise/main_app/admin.py
+++ Migrations_and_Django_Exercise/main_app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from main_app.models import EventRegistration, Movie, Student
+from main_app.models import EventRegistration, Movie, Student, Supplier
 
 
 # Register your models here.
@@ -32,3 +32,14 @@ class StudentAdmin(admin.ModelAdmin):
             'fields': ('grade',)
         }),
     )
+
+@admin.register(Supplier)
+class SupplierAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone')
+    list_filter = ('name', 'phone')
+    search_fields = ('email', 'contact_person', 'phone')
+    list_per_page = 20
+    fieldsets = ('Information', {
+        'fields':
+            ('name', 'contact_person', 'email', 'address')}
+                 )

882cca1fbbcf11e3522af31e217d35ce76604d18
Gloriya Gerginova <122824646+GloriyaSG@users.noreply.github.com>
2023-11-06 13:53:17 +0200

    migrate updated models

--- Data_Operations_in_Django_with_Queries_Lab/orm_skeleton_lab_4/caller.py
+++ Data_Operations_in_Django_with_Queries_Lab/orm_skeleton_lab_4/caller.py
@@ -7,5 +7,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
 # Import your models
+from main_app.models import Student
+
 # Create and check models
 # Run and print your queries
--- Data_Operations_in_Django_with_Queries_Lab/orm_skeleton_lab_4/orm_skeleton/settings.py
+++ Data_Operations_in_Django_with_Queries_Lab/orm_skeleton_lab_4/orm_skeleton/settings.py
@@ -76,15 +76,16 @@ WSGI_APPLICATION = 'orm_skeleton.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "",
-        "USER": "",
-        "PASSWORD": "",
-        "HOST": "",
-        "PORT": "",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Data_Operations_in_Django_with_Queries_Lab_db',
+        'USER': 'postgres-user',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation

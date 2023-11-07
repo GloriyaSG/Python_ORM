79b62e3824271f1bd710ea0d371bf644cb867b52
Gloriya Gerginova <122824646+GloriyaSG@users.noreply.github.com>
2023-11-07 22:20:29 +0200

    migrate updated models

--- Data_Operations_in_Django_with_Queries_Exercise/4_skeleton_exercise/orm_skeleton/settings.py
+++ Data_Operations_in_Django_with_Queries_Exercise/4_skeleton_exercise/orm_skeleton/settings.py
@@ -77,11 +77,14 @@ WSGI_APPLICATION = 'orm_skeleton.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Data_Operations_in_Django_with_Queries_Exercise_db',
+        'USER': 'postgres-user',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators

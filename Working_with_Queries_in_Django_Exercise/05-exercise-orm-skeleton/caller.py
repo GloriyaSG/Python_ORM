2c2d4853def09637a054f856005faea0615cbb10
Gloriya Gerginova <122824646+GloriyaSG@users.noreply.github.com>
2023-11-13 22:50:33 +0200

    migrate updated models

--- Working_with_Queries_in_Django_Exercise/05-exercise-orm-skeleton/caller.py
+++ Working_with_Queries_in_Django_Exercise/05-exercise-orm-skeleton/caller.py
@@ -6,5 +6,28 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
 # Import your models
+from main_app.models import ArtworkGallery
 # Create and check models
+def show_highest_rated_art():
+    art = ArtworkGallery.objects.all().order_by('-rating', 'id').first()
+    return f'{art.art_name} is the highest-rated art with {art.rating} rating!'
+
+def bulk_create_arts(first_art, second_art):
+    ArtworkGallery.objects.bulk_create([
+        first_art,
+        second_art,
+    ])
+
+def delete_negative_rated_arts():
+    ArtworkGallery.objects.filter(rating__lt=0).delete()
+
+
 # Run and print your queries
+# artwork1 = ArtworkGallery(artist_name="Vincent van Gogh", art_name="Starry Night", rating=4, price=1200000.0)
+# artwork2 = ArtworkGallery(artist_name="Leonardo da Vinci", art_name="Mona Lisa", rating=5, price=1500000.0)
+#
+# # Bulk saves the instances
+# bulk_create_arts(artwork1, artwork2)
+# print(show_highest_rated_art())
+# print(ArtworkGallery.objects.all())
+
--- Working_with_Queries_in_Django_Exercise/05-exercise-orm-skeleton/orm_skeleton/settings.py
+++ Working_with_Queries_in_Django_Exercise/05-exercise-orm-skeleton/orm_skeleton/settings.py
@@ -77,11 +77,14 @@ WSGI_APPLICATION = 'orm_skeleton.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Working_with_queries_in_django_ex_db',
+        'USER': 'postgres-user',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators

546ea40e9e492c4125f06aa7276795bb7ccfc8e3
Gloriya Gerginova <122824646+GloriyaSG@users.noreply.github.com>
2023-12-09 22:53:19 +0200

    migrate updated models

--- Exam Prep 1/orm_exam_skeleton/orm_skeleton/settings.py
+++ Exam Prep 1/orm_exam_skeleton/orm_skeleton/settings.py
@@ -77,10 +77,15 @@ WSGI_APPLICATION = 'orm_skeleton.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'exam_1_db',
+        'USER': 'postgres-user',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
